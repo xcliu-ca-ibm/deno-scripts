bb49a3192c8193c2f436df548368c8fda22329c7
Xinchun Liu <xcliu@ca.ibm.com>
2021-01-25 15:42:25 -0500

    adjust for travis notification

--- slack_notification.js
+++ slack_notification.js
@@ -12,12 +12,12 @@ let message = Deno.env.get("SLACK_MESSAGE") || "*you can specify message via env
 
 // specific for catalog build notification
 const CATALOG_IMAGE = Deno.env.get("CATALOG_IMAGE")
-const CATALOG_TAG = Deno.env.get("CATALOG_TAG")
+const CATALOG_TAG = Deno.env.get("CATALOG_TAG") || "latest"
 const DATESTAMP = Deno.env.get("DATESTAMP")
 if (CATALOG_IMAGE === "ibm-common-service-catalog") {
 	message = `*A new catalog build for BedRock has been promoted: by Travis <${Deno.env.get("TRAVIS_BUILD_WEB_URL").replace('https://', 'https://travis.ibm.com')}|build ${Deno.env.get("TRAVIS_BUILD_NUMBER")}>*
 - \`hyc-cloud-private-daily-docker-local.artifactory.swg-devops.com/ibmcom/${CATALOG_IMAGE}:${CATALOG_TAG}\``
-	if (DATESTAMP) {
+	if (CATALOG_TAG === "latest" && DATESTAMP) {
 		message += `
 - \`hyc-cloud-private-daily-docker-local.artifactory.swg-devops.com/ibmcom/${CATALOG_IMAGE}:${DATESTAMP}\`
 `
