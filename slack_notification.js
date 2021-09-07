c3428c4c74d415709e5d8e17b422f297ecec08bd
Xinchun Liu <xcliu@ca.ibm.com>
2021-09-07 11:36:33 -0400

    test bvt notify

--- slack_notification.js
+++ slack_notification.js
@@ -7,42 +7,49 @@ if (!token) {
 }
 const slackbot = new Slackbot(token)
 
-const channel = Deno.env.get("SLACK_CHANNEL") || "#cicd-fvt-reports"
+const channel = Deno.env.get("SLACK_CHANNEL") || "#icp-cicd-bots"
 let message = Deno.env.get("SLACK_MESSAGE") || "*you can specify message via environment variable SLACK_MESSAGE*"
 
+// specific for bvt notification
+const TO = Deno.env.get("TO")
+if (TO) {
+        message = `*${TO}_bvt failed for ${TRAVIS_BRANCH} by Travis Job <${Deno.env.get("TRAVIS_JOB_WEB_URL").replace('https://', 'https://travis.ibm.com')}|job ${Deno.env.get("TRAVIS_JOB_NUMBER")}>*\n`
+}
 // specific for catalog build notification
-const CATALOG_DIGEST = Deno.env.get("CATALOG_DIGEST")
-const AMD_DIGEST = Deno.env.get("AMD_DIGEST")
-const PPC_DIGEST = Deno.env.get("PPC_DIGEST")
-const S390_DIGEST = Deno.env.get("S390_DIGEST")
 const CATALOG_IMAGE = Deno.env.get("CATALOG_IMAGE")
-const CATALOG_TAG = Deno.env.get("CATALOG_TAG") || "cd"
-const DATESTAMP = Deno.env.get("DATESTAMP")
-if (CATALOG_IMAGE === "ibm-common-service-catalog") {
-	message = `*A new catalog build for BedRock has been promoted: by Travis <${Deno.env.get("TRAVIS_BUILD_WEB_URL").replace('https://', 'https://travis.ibm.com')}|build ${Deno.env.get("TRAVIS_BUILD_NUMBER")}>*
+if (CATALOG_IMAGE) {
+    const CATALOG_DIGEST = Deno.env.get("CATALOG_DIGEST")
+    const AMD_DIGEST = Deno.env.get("AMD_DIGEST")
+    const PPC_DIGEST = Deno.env.get("PPC_DIGEST")
+    const S390_DIGEST = Deno.env.get("S390_DIGEST")
+    const CATALOG_TAG = Deno.env.get("CATALOG_TAG") || "cd"
+    const DATESTAMP = Deno.env.get("DATESTAMP")
+    if (CATALOG_IMAGE === "ibm-common-service-catalog") {
+        message = `*A new catalog build for BedRock has been promoted: by Travis <${Deno.env.get("TRAVIS_BUILD_WEB_URL").replace('https://', 'https://travis.ibm.com')}|build ${Deno.env.get("TRAVIS_BUILD_NUMBER")}>*
 - \`hyc-cloud-private-daily-docker-local.artifactory.swg-devops.com/ibmcom/${CATALOG_IMAGE}:${CATALOG_TAG}\``
-	if (DATESTAMP && ["cd", "efix", "eus"].find(e => e === CATALOG_TAG)) {
-		message += `
+        if (DATESTAMP && ["cd", "efix", "eus"].find(e => e === CATALOG_TAG)) {
+            message += `
 - \`hyc-cloud-private-daily-docker-local.artifactory.swg-devops.com/ibmcom/${CATALOG_IMAGE}:${DATESTAMP}\`
 `
-	}
-	if (CATALOG_DIGEST) {
-		message += `
+        }
+        if (CATALOG_DIGEST) {
+            message += `
 Catalog digest: \`${CATALOG_DIGEST}\`
 `
-	}
-	if (AMD_DIGEST) {
-		message += `amd64 digest: \`${AMD_DIGEST}\`
+        }
+        if (AMD_DIGEST) {
+            message += `amd64 digest: \`${AMD_DIGEST}\`
 `
-	}
-	if (PPC_DIGEST) {
-		message += `ppc64le digest: \`${PPC_DIGEST}\`
+        }
+        if (PPC_DIGEST) {
+            message += `ppc64le digest: \`${PPC_DIGEST}\`
 `
-	}
-	if (S390_DIGEST) {
-		message += `s390x digest: \`${S390_DIGEST}\`
+        }
+        if (S390_DIGEST) {
+            message += `s390x digest: \`${S390_DIGEST}\`
 `
-	}
+        }
+    }
 }
 
 await slackbot.sendMessage(`${channel}`, `${message}`);
