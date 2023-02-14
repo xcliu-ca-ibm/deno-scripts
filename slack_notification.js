31321565051183834b25304398c8f834bb21ec0e
Xinchun Liu <xcliu@ca.ibm.com>
2023-02-14 15:57:08 -0500

    notify 323

--- slack_notification.js
+++ slack_notification.js
@@ -18,6 +18,10 @@ const CS_VERSIONS = {
     "efix-daily": "3.20.2",
     "efix-quay": "3.20.2",
     "efix-icr": "3.20.2",
+    "323": "3.23.1",
+    "323-daily": "3.23.1",
+    "323-quay": "3.23.1",
+    "323-icr": "3.23.1",
     future: "3.99.0",
     "future-daily": "3.99.0",
     "future-quay": "3.99.0",
@@ -95,7 +99,7 @@ if (CATALOG_IMAGE) {
     if (CATALOG_IMAGE === "ibm-common-service-catalog") {
         message = `*CS ${CS_VERSIONS[CATALOG_TAG]} (\`${CATALOG_TAG}\`) catalog build for BedRock has been promoted: by Travis <${Deno.env.get("TRAVIS_BUILD_WEB_URL", "test").replace('https://', 'https://travis.ibm.com')}|build ${Deno.env.get("TRAVIS_BUILD_NUMBER")}>*
 - \`docker-na-public.artifactory.swg-devops.com/hyc-cloud-private-daily-docker-local/ibmcom/${CATALOG_IMAGE}:${CATALOG_TAG}\``
-        if (DATESTAMP && ["cd", "ltsr", "efix", "eus", "future"].find(e => e === CATALOG_TAG)) {
+        if (DATESTAMP && ["cd", "ltsr", "323", "efix", "eus", "future"].find(e => e === CATALOG_TAG)) {
             message += `
 - \`docker-na-public.artifactory.swg-devops.com/hyc-cloud-private-daily-docker-local/ibmcom/${CATALOG_IMAGE}:${DATESTAMP}\``
             message += `
